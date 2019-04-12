d4a883688b11fdf27a8ee912092485af3f61e640
jabbard <Potter123>
2019-04-12 12:52:57 +0545

    Add project files.

---
+++ AppDevGW/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AppDevGW.Startup))]
+namespace AppDevGW
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
